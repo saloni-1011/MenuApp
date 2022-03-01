efd481507a7991b8de7aaf570045aaeb9c621a5b
saloni-1011 <saloni1940.cse19@chitkara.edu.in>
2022-03-01 15:13:09 +0530

    Add all my files

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,308 @@
+const Menu = [
+    //Indian
+    
+    {
+      id: 1,
+      image: "images/allupakoida.jpg",
+      name: "Aloo Pakoda",
+      category: "Indian",
+      price: "20₹",
+      description:
+        "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks.",
+    },
+    {
+      id: 2,
+      image: "../images/samosa.jpg",
+      name: "Samosa",
+      category: "Indian",
+      price: "10₹",
+      description:
+        "A samosa is a fried or baked pastry with a savory filling, including ingredients such as spiced potatoes, onions, peas, chicken and/or other meats. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.",
+    },
+    {
+      id: 3,
+      image: "../images/dhokla.jpg",
+      name: "Dhokla",
+      category: "Indian",
+      price: "10₹",
+      description:
+        "Hailed as the regional dish of northwest India, the Gujarati delicacy dhokla is a savoury vegetarian snack made of rice and split chickpeas. It’s tastier than it sounds – Gujaratis eat it for breakfast or lunch, and sometimes even as a snack or side dish.",
+    },
+    {
+      id: 4,
+      image: "../images/vadapao.jpg",
+      name: "Vada Pao",
+      category: "Indian",
+      price: "10₹",
+      description:
+        "Originating in the traditionally vegetarian state of Maharashtra, vada pav is as close as Indian cuisine gets to veggie burgers. One for carb lovers, vada pav consists of a deep fried potato dumpling placed neatly inside a small bun. The finger food delicacy is generally accompanied by a couple of chutneys and a green chilli, to appeal to the spice loving palettes of Indians up and down the country. Also called a Bombay burger.",
+    },
+    {
+      id: 5,
+      image: "../images/paratha.jpg",
+      name: "Stuffed Paratha",
+      category: "Indian",
+      price: "10₹",
+      description:
+        "Punjab’s foodie heritage doesn’t stop at dal makhani. Often eaten at the start of the day, stuffed parathas are seen as the breakfast of champions in northern India. The word paratha derives from the Sanskrit word atta meaning ‘layers of cooked dough,’ and this dish lives up to its moniker.Parathas can be stuffed with any number of fillings, but some of our favourites are aloo paratha (stuffed with potatoes) and methi paratha (stuffed with fenugreek). ",
+    },
+    {
+      id: 6,
+      image: "../images/chai.jpg",
+      name: "Masala Chai",
+      category: "Indian",
+      price: "10₹",
+      description:
+        "India’s most famous export, masala chai can be found being sold by everywhere from high end restaurants to chaiwallas at train stations. While there’s many different diluted versions of this classic Indian tea around the globe, the real deal can only be found in India. Authentic masala chai is made by brewing black tea on the stove with a mixture of aromatic spices and herbs.",
+    },
+    {
+      id: 7,
+      image: "../images/dosa.jpg",
+      name: "Dosa",
+      category: "Indian",
+      price: "10₹",
+      description:
+        "Arguably South India’s most renowned culinary export, masala dosas are famous the world over. A sort of Indian pancake, dosas are made from a thin batter consisting of rice, flour and lentils.",
+    },
+    {
+      id: 8,
+      image: "../images/paubhaji.jpg",
+      name: "Pao Bhaji",
+      category: "Indian",
+      price: "10₹",
+      description:
+        "Pav bhaji is a fast food dish from India consisting of a thick vegetable curry served with a soft bread roll. Its origins are in the state of Maharashtra.",
+    },
+    {
+      id: 9,
+      image: "../images/chola.jpg",
+      name: "Choley Bhature",
+      category: "Indian",
+      price: "50₹",
+      description:
+        "Chole bhature is a food dish popular in the Northern areas of the Indian subcontinent. It is a combination of chana masala and bhatura/puri, a fried bread made from maida. Although it is known as a typical Punjabi dish, there are varied claims around the origin of dish.",
+    },
+    {
+      id: 10,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Chawal",
+      category: "Indian",
+      price: "60₹",
+      description:
+        "Rājmā, also known as rajmah, rāzmā, or lal lobia, is a vegetarian dish, originating from the Indian subcontinent, consisting of red kidney beans in a thick gravy with many Indian whole spices, and is usually served with rice. It is a part of regular diet in India",
+    },
+    
+    {
+      id: 11,
+      image: "../images/vegthali.jpg",
+      name: "Veg-Thali",
+      category: "Indian",
+      price: "100₹",
+      description:
+        "The rustic and robust Oriya thali, with its emphasis on copious amounts of vegetables and restrained use of spices and oil,  is among the healthiest thalis in India. The thali includes roti, rice in fermented and spiced curd, signature lentil and vegetables dish, bitter gourd fries, potatoes with poppy seeds,roasted brinjal mash,dry cauliflower curry,sweet sour tomato and date chutney, and the divinely delicious kanika or meethi khechudiya.",
+    },
+    {
+      id: 12,
+      image: "../images/rajhasthan.jpg",
+      name: "Rajasthani Thali",
+      category: "Indian",
+      price: "180₹",
+      description:
+        "A traditional Rajasthani thali is an elaborate spread of gourmet preparations that would delight one and all. The local hits on this thali include dal baati churma , missi roti , gatte ki sabzi , combination of  5 types of lentils, fiery meat curry, goond ka laddoo (a kind of sweet), flavoured buttermilk. To add another delicious level to the meal, there are salads, papads, pickles, and chutneys to choose from.",
+    },
+    {
+      id: 13,
+      image: "../images/gulabjamun.jpg",
+      name: "Gulab Jamun",
+      category: "Indian",
+      price: "40₹",
+      description:
+        "Gulab jamun is a milk-solid-based sweet, originating in the Indian subcontinent and a type of mithai popular in India.",
+    },
+    //Japanese
+    {
+      id: 1,
+      image: "../images/sushiroll.jpg",
+      name: "Sushi",
+      category: "Japanese",
+      price: "210₹",
+      description:
+        "Sushi is one of the best known Japanese foods around the world. It is offered in various ways and prices, from the entertaining kaiten-zushi (conveyor belt sushi), where visitors can enjoy sushi for a reasonable price of about 100 yen per plate, to high-end, long-established, traditional Edomae sushi (Edo-style sushi) where you will sit at a quiet counter to eat as the sushi is prepared right before your eyes. ",
+    },
+    {
+      id: 2,
+      image: "../images/unagi.jpg",
+      name: "Unagi - Grilled Eel",
+      category: "Japanese",
+      price: "210₹",
+      description:
+        "Unagi, or eel, is a fish known to be found mainly in rivers. In Japan, it is a delicacy typical in high-class Japanese dining. There are also many casual restaurants that specialize in unagi dishes. At unagi restaurants, you will be able to enjoy kabayaki, where the unagi is put on skewers and grilled with a special sauce containing soy sauce, mirin, sugar, and sake. Unadon, a dish of kabayaki on top of white rice, is also offered at these establishments.",
+    },
+    {
+      id: 3,
+      image: "../images/onigiri.jpg",
+      name: "Onigiri - Rice Balls",
+      category: "Japanese",
+      price: "210₹",
+      description:
+        "Onigiri, also called omusubi, may just look like plain rice, but they often have a savory filling inside and are wrapped with a salty sheet of nori seaweed. They are made in bento lunches by families and often seen sold in convenience stores and supermarkets. This is a classic choice for a snack or light meal.",
+    },
+    {
+      id: 4,
+      image: "../images/misosoup.jpg",
+      name: "Miso Soup",
+      category: "Japanese",
+      price: "210₹",
+      description:
+        "Miso soup is another famous Japanese food, renowned for its great taste and health benefits. This soup is conventionally drunk accompanied by other side and main dishes. A traditional Japanese diet generally includes drinking miso soup daily.",
+    },
+    {
+      id: 5,
+      image: "../images/soba.jpg",
+      name: "Soba",
+      category: "Japanese",
+      price: "210₹",
+      description:
+      "Soba is a noodle dish made from buckwheat flour with water and flour, thinly spread and cut into noodles with widths of 1cm-2cm. After boiling the noodles in hot water, it is eaten dipped in cold soup, or by pouring hot soup over it. The soba broth (tsuyu) is typically made from kombu or dried bonito broth, seasoned with soy sauce and mirin, and is crucial for having a delicious soba experience.Soba is enjoyed hot or cold, making it an ideal dish year-round",
+        
+    },
+    //Chinese
+    {
+      id: 1,
+      image: "images/dimsum.jpg",
+      name: "Dim Sums",
+      category: "Chinese",
+      price: "30₹",
+      description:
+        "One of the most loves street food from the north east, widely popular across northern India now comes to your kitchen! Small bite-sized rounds stuffed with veggies or meat. Dim Sums are perfect steamed snack to delight those evening cravings.",
+    },
+    {
+      id: 2,
+      image: "images/springroll.jpg",
+      name: "Spring Rolls",
+      category: "Chinese",
+      price: "30₹",
+      description:
+        "A crisp appetizer where shredded veggies are encased in thin sheets and then fried golden. Little munchies to prepare at home for a high tea menu or just a party starter, serve with a tangy dip",
+    },
+    {
+      id: 3,
+      image: "images/friedrice.jpg",
+      name: "Fried Rice",
+      category: "Chinese",
+      price: "30₹",
+      description:
+        "Fried rice is a very simple but popular Chinese cuisine. It is a dish of boiled rice that is usually mixed with scallions and minced meat and quickly scrambled with eggs.",
+    },
+    {
+      id: 4,
+      image: "images/peking-duck.jpg",
+      name: "Peking Duck",
+      category: "Chinese",
+      price: "30₹",
+      description:
+        "Peking Roast Duck is a renowned Beijing dish with a worldwide reputation. The high-quality duck meat, roasted using wood charcoal, looks reddish, with crisp skin and tender meat, and is known as “one of heaven’s delicacies”.",
+    },
+    {
+      id: 5,
+      image: "images/chilli-chicken.jpg",
+      name: "Szechwan Chilli Chicken",
+      category: "Chinese",
+      price: "30₹",
+      description:
+        "A fiery delight straight from the Sichuan region. It is loaded with pungent spices like brown pepper. red chillies, ginger, green chillies and white pepper.",
+    },
+    {
+      id: 6,
+      image: "images/noodles.jpg",
+      name: "Noodles",
+      category: "Chinese",
+      price: "30₹",
+      description:
+        "One of the staples in every home, noodles are not just a kid's favourite snack to binge on but are also equally loved by adults. Here is a noodles recipe that is super quick and easy to prepare at home. Just bung in all your favourite veggies and create a masterpiece of your own.",
+    },
+    {
+      id: 7,
+      image: "images/hotnsour.jpg",
+      name: "Hot n Sour Soup",
+      category: "Chinese",
+      price: "30₹",
+      description:
+        "Isn't it great to warm up with a piping hot bowl of soup during the winters? Here is a soup with a spicy and sour broth. It is made with the goodness of mushrooms, cabbage, carrot and a spicy twist of red peppers or white pepper and sour with vinegar.",
+    },
+    //Italian
+    {
+      id: 1,
+      image: "images/lasagna.jpg",
+      name: "Lasagna Bolognese",
+      category: "Italian",
+      price: "20₹",
+      description:
+        "Lasagna Bolognese is a typical Sunday lunch or main course meal, and adults and children appreciate it for its rich and tasty flavour. It’s made with fresh layers of lasagne sheets, bechamel sauce, Bolognese sauce, and plenty of parmesan cheese. Whatever the occasion, you can’t go wrong with a classic Lasagne Bolognese.",
+    },
+    {
+      id: 2,
+      image: "images/Gnocchi.jpg",
+      name: "Gnocchi Sorrento",
+      category: "Italian",
+      price: "20₹",
+      description:
+        "Gnocchi Alla Sorrentina is a great comfort food dish made with soft potato gnocchi, baked in a creamy tomato sauce and mozzarella cheese. The soft gnocchi is lightly baked, and the bubbling creamy cheese and mozzarella are hard to beat.",
+    },
+    {
+      id: 3,
+      image: "images/carbonara.jpg",
+      name: "Spaghetti Carbonara",
+      category: "Italian",
+      price: "20₹",
+      description:
+        "Carbonara is another classic Italian pasta dish made with simple ingredients and bursting with flavour. It is made with egg, cured pork, hard cheese, and black pepper. The cheese is usually Pecorino Romano or Parmigiano-Reggiano.",
+    },
+    {
+      id: 4,
+      image: "images/antipasto.jpg",
+      name: "Antipasto Italiano",
+      category: "Italian",
+      price: "20₹",
+      description:
+        "Antipasto means “before the meal” and is a combination of small bites of tasty food – usually combined with wine or beer to stimulate your appetite before the main meal.",
+    },
+    {
+      id: 5,
+      image: "images/alfredo.jpg",
+      name: "Fettuccine Alfredo",
+      category: "Italian",
+      price: "20₹",
+      description:
+        "Fettuccine Alfredo is a fresh pasta dish made with fettuccine, butter, and Parmesan cheese. As the cheese melts, it forms a smooth and creamy sauce coating the pasta. You can use Alfredo to coat broccoli and partner with crispy chicken.",
+    },
+    {
+      id: 6,
+      image: "images/ravioli.jpg",
+      name: "Ravioli",
+      category: "Italian",
+      price: "20₹",
+      description:
+        "Ravioli is a type of pasta made with filling, usually served in broth or with a sauce.",
+    },
+    {
+      id: 7,
+      image: "images/pizza.jpg",
+      name: "Pizza Margherita",
+      category: "Italian",
+      price: "20₹",
+      description:
+        "3.	To get a full authentic Italian experience, you have to go with the Margherita. Simple, fresh, and flavourful, Pizza Margherita is made with tomatoes, mozzarella, fresh basil, and olive oil and baked at high temperatures for a crispy-on-the-outside crust.",
+    },
+    {
+      id: 8,
+      image: "images/tiramisu.jpg",
+      name: "Tiramisu",
+      category: "Italian",
+      price: "20₹",
+      description:
+        "This Italian dessert is made from ladyfinger sponge cookies soaked in espresso and rum and layered with a whipped mixture of eggs, sugar, mascarpone cheese, and topped with cocoa.",
+    },
+  ];
+  
+  export default Menu;
